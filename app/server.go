4b25e1238d12eb93add31c6c8e71f3b2bd413d8a
salehzaidan <salehzaidan64@gmail.com>
2024-03-16 17:25:49 +0700

    Initialize TCP server

--- app/server.go
+++ app/server.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"log"
+	"net"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:4221")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 4221")
-	// 	os.Exit(1)
-	// }
-	//
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 }
